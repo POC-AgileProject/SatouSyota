917738d47d5e380c8811130bd7ca234cce3ec00a
MasahiroYoshikoshi <m.yoshikoshi106@gmail.com>
2018-12-06 17:15:41 +0900

    add:共通変数classの追加

---
+++ app/src/main/java/rd/slcs/co/jp/showtabi/common/Const.java
@@ -0,0 +1,10 @@
+package rd.slcs.co.jp.showtabi.common;
+
+public class Const {
+
+    /** Plans　データベース名 */
+    public static final String DB_PLANTABLE = "plans";
+
+    public static final String DB_PLANTABLE_STARTYMD = "startYMD";
+
+}
