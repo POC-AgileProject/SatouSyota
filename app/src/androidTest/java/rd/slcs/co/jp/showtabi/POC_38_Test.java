99d12857b4cb3975636da833829170c4c077a07c
MasahiroYoshikoshi <m.yoshikoshi106@gmail.com>
2018-12-26 14:15:51 +0900

    【mod】ExpressoテストがCircleCI上でエラーとなる課題に対応。

--- app/src/androidTest/java/rd/slcs/co/jp/showtabi/POC_38_Test.java
+++ app/src/androidTest/java/rd/slcs/co/jp/showtabi/POC_38_Test.java
@@ -20,6 +20,7 @@ import rd.slcs.co.jp.showtabi.object.Plan;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -92,14 +93,19 @@ public class POC_38_Test {
         }
         // 登録するプラン情報を入力
         onView(withId(R.id.editPlanName))
+                .perform(scrollTo())
                 .perform(replaceText("テストプラン"));
         onView(withId(R.id.editStartDay))
+                .perform(scrollTo())
                 .perform(replaceText("20500101"));
         onView(withId(R.id.editEndDay))
+                .perform(scrollTo())
                 .perform(replaceText("20510101"));
         onView(withId(R.id.editPerson))
+                .perform(scrollTo())
                 .perform(replaceText("100"));
         onView(withId(R.id.editMemo))
+                .perform(scrollTo())
                 .perform(replaceText("テストメモ"));
         // 保存ボタンを押下
         onView(withId(R.id.button_save))
