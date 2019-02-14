d34933f88a61bc6371454e14189090b263f785a5
theMistletoe <katachiwotodomete@gmail.com>
2019-02-14 10:47:44 +0900

    fix:IDの修正

--- app/src/main/java/rd/slcs/co/jp/showtabi/activity/MainActivity.java
+++ app/src/main/java/rd/slcs/co/jp/showtabi/activity/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView topLogo = findViewById(R.id.carlos);
+        ImageView topLogo = findViewById(R.id.topLogo);
         fadein(topLogo);
 
         new Handler().postDelayed(new Runnable(){
