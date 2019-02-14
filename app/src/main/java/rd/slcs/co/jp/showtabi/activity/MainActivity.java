d1f969cb28dcf4d4f569165a5cddcaf583d2a1a0
theMistletoe <katachiwotodomete@gmail.com>
2019-02-14 10:46:30 +0900

    fix:top画像をtop画面に表示
    top画像をフェードインする実装を追加
    文言を削除する

--- app/src/main/java/rd/slcs/co/jp/showtabi/activity/MainActivity.java
+++ app/src/main/java/rd/slcs/co/jp/showtabi/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
 
 import rd.slcs.co.jp.showtabi.R;
 
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView topLogo = findViewById(R.id.carlos);
+        fadein(topLogo);
 
         new Handler().postDelayed(new Runnable(){
 
@@ -26,5 +31,16 @@ public class MainActivity extends AppCompatActivity {
 
         }, 2000);
 
+    }
+
+    private void fadein(ImageView image){
+        // 透明度を0から1に変化
+        AlphaAnimation alphaFadeIn = new AlphaAnimation(0.0f, 1.0f);
+        // animation時間 msec
+        alphaFadeIn.setDuration(1500);
+        // animationが終わったそのまま表示にする
+        alphaFadeIn.setFillAfter(true);
+
+        image.startAnimation(alphaFadeIn);
     }
 }
