cbbb9aa20df9dc47645f64626d00a749511485b9
N_WATADA <wtd-3a.7038@hotmail.co.jp>
2019-02-15 14:49:02 +0900

    【FIX】終了時間を必須項目に設定。

--- app/src/main/java/rd/slcs/co/jp/showtabi/activity/EventAddActivity.java
+++ app/src/main/java/rd/slcs/co/jp/showtabi/activity/EventAddActivity.java
@@ -93,11 +93,12 @@ public class EventAddActivity extends AppCompatActivity {
                 EditText editMemo = findViewById(R.id.editMemo);
                 EditText editAddress = findViewById(R.id.editAddress);
 
-                //TODO:開始日と終了日の前後チェック
+
                 // 入力チェック
                 if ("".equals(editEventName.getText().toString())
                         || "".equals(editEventDate.getText().toString())
-                        || "".equals(editStartTime.getText().toString())) {
+                        || "".equals(editStartTime.getText().toString())
+                        || "".equals(editEndTime.getText().toString())) {
 
                     Toast.makeText(this, R.string.msg_error_0001, Toast.LENGTH_LONG).show();
 
@@ -105,11 +106,8 @@ public class EventAddActivity extends AppCompatActivity {
 
                     // 日付と時間の連結
                     String startTime = editEventDate.getText().toString() + editStartTime.getText().toString();
-                    String endTime = "";
-                    // 終了時間が入力されている場合
-                    if (!"".equals(editEndTime.getText().toString())) {
-                        endTime = editEventDate.getText().toString() + editEndTime.getText().toString();
-                    }
+                    String endTime = editEventDate.getText().toString() + editEndTime.getText().toString();
+
 
                     Event event = new Event();
 
